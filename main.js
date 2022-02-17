622a493859419a8e085f9061eb0a7829fa702e65
Olivia Whitehead <oliviawhitehead3@gmail.com>
2022-02-17 14:29:04 -0700

    Add more form functionality

--- main.js
+++ main.js
@@ -4,11 +4,10 @@ var dishPrinted = document.querySelector('.dish-output');
 var dishHeading = document.querySelector('.dish-heading');
 var clearButton = document.querySelector('.clear');
 
-// var currentSide = sides[getRandomIndex(sides)];
-// var currentMain = mains[getRandomIndex(mains)];
-// var currentDessert = desserts[getRandomIndex(desserts)];
-
-letsCookButton.addEventListener('click', showRandomDish);
+letsCookButton.addEventListener('click', function() {
+  showRandomDish();
+  showEntireMeal();
+});
 clearButton.addEventListener('click', showHomePage);
 
 function getRandomIndex(array) {
@@ -31,6 +30,16 @@ function showRandomDish() {
   event.preventDefault();
 };
 
+// function showEntireMeal() {
+//   var selectedButton = document.querySelector('input[name="food"]:checked').value;
+//   if (selectedButton === 'entire-meal') {
+//     dishPrinted.innerHTML =
+//       <p>sides[getRandomIndex(sides)] + "!"</p>,
+//       <p>mains[getRandomIndex(mains)] + "!",</p>,
+//       <p>desserts[getRandomIndex(desserts)] + "!"</p>
+//   }
+// }
+
 function showHomePage() {
   cookpotImage.classList.remove('hidden');
   dishPrinted.classList.add('hidden');
@@ -39,35 +48,14 @@ function showHomePage() {
   event.preventDefault();
 }
 
-
-
-
-// function showRandomDish() {
-//   var dish = document.querySelector(".radio").value;
-//   document.querySelector(".dish-output").innerText = dish;
-// }
-
-
-// function showRandomDish() {
-//   event.preventDefault();
-//   var food = document.form[0];
-//   var txt = '';
-//
-//   for(let i = 0; i < food.length; i++) {
-//     if (food[i].checked) {
-//       txt = txt + food[i].value;
-//     }
-//   }
-//   document.querySelctor('#output').innerText(`Show ${txt}`);
-// }
-
-
 var sides = ['Miso Glazed Carrots', 'Coleslaw', 'Garden Salad', 'Crispy Potatoes',
 'Sweet Potato Tots', 'Coconut Rice', 'Caesar Salad', 'Shrimp Summer Rolls',
 'Garlic Butter Mushrooms', 'Hush Puppies'];
+
 var mains = ['Spaghetti and Meatballs', 'Pineapple Chicken', 'Shakshuka', 'Thai Yellow Curry',
 'Bibimbap', 'Chicken Parmesan', 'Butternut Squash Soup', 'BBQ Chicken Burgers',
 'Ramen', 'Empanadas', 'Chicken Fried Rice', 'Sheet Pan Fajitas', 'Margarita Pizza'];
+
 var desserts = ['Apple Pie', 'Lemon Meringue Pie', 'Black Forest Cake', 'Banana Bread',
 'Peach Cobbler', 'Cheesecake', 'Funfetti Cake', 'Baklava', 'Flan', 'Macarons', 'Macaroons',
 'Chocolate Cupcakes', 'Pavlova', 'Pumpkin Pie', 'Key Lime Pie', 'Tart Tatin',
